f12fb58f8ac6c67e42ef7d57d16809202f409caf
Allan <allan3723@gmail.com>
2026-02-19 11:20:36 -0800

    Fixed skip-worktree changes

--- PaymentsSample/Constants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constants.swift
-//  PaymentsSample
-//
-
-// REPLACE THESE VALUES
-let applePayMerchantId = "YOUR APPLE PAY MERCHANT ID"
-let token = "YOUR FISERV TOKEN"
-let locationId = "YOUR LOCATION ID"
